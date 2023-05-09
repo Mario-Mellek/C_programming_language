c52255ed9708d42f42824d866e8860ace5cec1b8
Mario Mellek <107096218+Mario-Mellek@users.noreply.github.com>
2023-05-09 18:16:30 +0000

    Tue, May 9, 2023, 8:16 PM +02:00

---
+++ conditionsAndLoops.py
@@ -0,0 +1,7 @@
+for i in range(1, 10):
+    if i <= 5:
+        print("AAAHHHH")
+    elif i >= 8 and i <= 8:
+        print("WHERE ALL THE BRACKETS??")
+    else:
+        print("HOW DO YOU READ THIS!!!")
