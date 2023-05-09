527f124d6865e434dfe6314cf71a30e2a555fca6
Mario Mellek <107096218+Mario-Mellek@users.noreply.github.com>
2023-05-09 21:49:58 +0000

    Tue, May 9, 2023, 11:49 PM +02:00

--- test.py
+++ test.py
@@ -24,12 +24,29 @@ for i in range(2, 4):
 for i in range(4):
     print(i, end=" ")
 
-print("\n----------------")
-
 a = 12
 if a < 2:
     print("Holberton")
 elif a % 2 == 0:
     print("C is fun")
+else:
+    print("School")
+
+print("\n----------------")
+
+
+if 12 == 48/3 or 12 is 12:
+    print("Holberton")
+else:
+    print("School")
+
+
+
+a = 12
+if a > 2:
+    if a % 2 == 0:
+        print("Holberton")
+    else:
+        print("C is fun")
 else:
     print("School")
