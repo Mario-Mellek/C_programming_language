495444ab1a52a1bfff7f12798fef883945028d98
Mario Mellek <107096218+Mario-Mellek@users.noreply.github.com>
2023-05-09 18:25:17 +0000

    Tue, May 9, 2023, 8:25 PM +02:00

--- conditionsAndLoops.py
+++ conditionsAndLoops.py
@@ -13,3 +13,14 @@ print(len(name))
 
 for i in range(len(name)):
     print(f"Here: {i}")
+
+print("-------------------------")
+
+for n in range(2, 10):
+    for x in range(2, n):
+        if n % x == 0:
+            print(n, 'equals', x, '*', n//x)
+            break
+    else:
+        # loop fell through without finding a factor
+        print(n, 'is a prime number')
