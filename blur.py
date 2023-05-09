c993ad5e133888b8fb161bb43c7687eb5eafa3b3
Mario Mellek <107096218+Mario-Mellek@users.noreply.github.com>
2023-05-09 14:30:59 +0000

    Tue, May 9, 2023, 4:30 PM +02:00

--- blur.py
+++ blur.py
@@ -1,4 +1,6 @@
 from PIL import Image, ImageFilter
 
 before = Image.open("image.jpg")
-after = before.filer(ImageFilter.BoxBlur(1))
+after = before.filter(ImageFilter.BoxBlur(50))
+
+after.save("out.jpg")
