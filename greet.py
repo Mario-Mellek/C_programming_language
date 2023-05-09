d9940b9c127b89d05c2659f3ef0ffdf511f619a0
Mario Mellek <107096218+Mario-Mellek@users.noreply.github.com>
2023-05-09 13:42:53 +0000

    Tue, May 9, 2023, 3:42 PM +02:00

--- greet.py
+++ greet.py
@@ -1,11 +1,19 @@
 from cs50 import get_string
 
 answer = get_string("What's your name?\n-> ")
-
 if answer == "Mario":
     print("Owner")
 else:
     print("User")
 
-print("Hello, " + answer)
 print(f"Hello, {answer}")
+
+
+number = 2
+
+if number > 5:
+    print("Greater than 5")
+elif number < 5:
+    print("less than 5")
+else:
+    print("Equal to 5")
