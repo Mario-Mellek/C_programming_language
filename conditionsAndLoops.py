5faa489db3f457f0d084535512f0cdf950332439
Mario Mellek <107096218+Mario-Mellek@users.noreply.github.com>
2023-05-09 18:19:41 +0000

    Tue, May 9, 2023, 8:19 PM +02:00

--- conditionsAndLoops.py
+++ conditionsAndLoops.py
@@ -1,7 +1,7 @@
 for i in range(1, 10):
     if i <= 5:
         print(f"5{i}")
-    elif i >= 8 and i <= 8:
+    elif i >= 6 and i <= 8:
         print(f"from 5 to 8 {i}")
     else:
         print(f"HOW DO YOU READ THIS!!!{i}")
