659b51c89ced1c28c43a104655fa7eabdfeccb44
Mario Mellek <107096218+Mario-Mellek@users.noreply.github.com>
2023-05-09 21:48:21 +0000

    Tue, May 9, 2023, 11:48 PM +02:00

--- test.py
+++ test.py
@@ -2,3 +2,34 @@ if True:
     print("True")
 else:
     print("False")
+
+
+for i in range(2, 10, 2):
+    print(i, end=" ")
+
+
+if 12 == 48/4:
+    print("Holberton")
+else:
+    print("School")
+
+if 12 == 48/4 and False:
+    print("Holberton")
+else:
+    print("School")
+
+for i in range(2, 4):
+    print(i, end=" ")
+
+for i in range(4):
+    print(i, end=" ")
+
+print("\n----------------")
+
+a = 12
+if a < 2:
+    print("Holberton")
+elif a % 2 == 0:
+    print("C is fun")
+else:
+    print("School")
