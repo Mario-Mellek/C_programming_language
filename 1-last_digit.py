c4723468947465848005d2d99bbade1556d4e33f
Mario Mellek <107096218+Mario-Mellek@users.noreply.github.com>
2023-05-09 23:59:21 +0000

    Wed, May 10, 2023, 1:59 AM +02:00

--- 1-last_digit.py
+++ 1-last_digit.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
+lastDigit = abs(number) % 10
 
+if number < 0:
+    print("last digit of", number, "is", -lastDigit, end=" ")
+if lastDigit > 5:
+    
