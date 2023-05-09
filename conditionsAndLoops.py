6f30166b3fd6db8924fbfdaba6f720b926235a65
Mario Mellek <107096218+Mario-Mellek@users.noreply.github.com>
2023-05-09 18:30:16 +0000

    Tue, May 9, 2023, 8:30 PM +02:00

--- conditionsAndLoops.py
+++ conditionsAndLoops.py
@@ -17,8 +17,9 @@ for i in range(len(name)):
 print("-------------------------")
 
 for n in range(2, 10):
+    print(f"n = {n}")
     for x in range(2, n):
-    print(n, x)
+        print(f:"x = {x}")
         if n % x == 0:
             print(n, 'equals', x, '*', n//x)
             break
