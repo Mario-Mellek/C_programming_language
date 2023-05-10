5f81b67688984ca93e8b85ca1ee409db6ae67420
Mario Mellek <107096218+Mario-Mellek@users.noreply.github.com>
2023-05-10 03:25:08 +0000

    Wed, May 10, 2023, 5:25 AM +02:00

--- functions/remove_char_at.py
+++ functions/remove_char_at.py
@@ -4,4 +4,8 @@ def remove_char_at(str, n):
     return str[:n] + str[n + 1 :]
 
 
-remove_char_at("Hello, World", 4)
+print(remove_char_at("Best School", 3))
+print(remove_char_at("Chicago", 2))
+print(remove_char_at("C is fun!", 0))
+print(remove_char_at("School", 10))
+print(remove_char_at("Python", -2))
