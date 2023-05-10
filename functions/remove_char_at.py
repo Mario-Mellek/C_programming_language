3a21c45e1fe7825a114bc0b4a97451e3af2c9828
Mario Mellek <107096218+Mario-Mellek@users.noreply.github.com>
2023-05-10 03:23:39 +0000

    Wed, May 10, 2023, 5:23 AM +02:00

--- functions/remove_char_at.py
+++ functions/remove_char_at.py
@@ -1,5 +1,7 @@
 def remove_char_at(str, n):
     if n < 0 or n >= len(str):
         return str
-    return str[:n] + str[n+1:]
+    return str[:n] + str[n + 1 :]
 
+
+remove_char_at("Hello, World", 4)
