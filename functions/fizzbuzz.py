fde6d221d584bff9d64fd09e6b2da191d033e258
Mario Mellek <107096218+Mario-Mellek@users.noreply.github.com>
2023-05-10 02:29:07 +0000

    Wed, May 10, 2023, 4:29 AM +02:00

--- functions/fizzbuzz.py
+++ functions/fizzbuzz.py
@@ -2,9 +2,13 @@
 
 def fizzbuzz():
     for i in range(1, 101):
-        if i % 3 = 0 and i % 5 = 0:
+        if i % 3 == 0 and i % 5 == 0:
             print("FizzBuzz", end=" ")
-        elif i % 3 = 0:
+        elif i % 3 == 0:
             print("Fizz", end=" ")
-        elif i % 5 = 0:
+        elif i % 5 == 0:
             print("Buzz", end=" ")
+        else:
+            print(i, end=" ")
+
+fizzbuzz()
