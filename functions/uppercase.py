1707901ae446fbb376117616a3bf3f1098afaa55
Mario Mellek <107096218+Mario-Mellek@users.noreply.github.com>
2023-05-10 01:34:02 +0000

    Wed, May 10, 2023, 3:34 AM +02:00

--- functions/uppercase.py
+++ functions/uppercase.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def uppercase(str):
-    for c in str:
+    strLen = len(str)
+    for i in range(strLen):
+        c = str[i]
         if ord(c) >= ord('a') and ord(c) <= ord('z'):
-            c = chr(ord(c) - 32)
-        print("{}".format(c), end="")
-    print("\n")
+            c = chr(ord(c - 32))
+        
