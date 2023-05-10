175a1d946631e8286113fe151613446c547e9efd
Mario Mellek <107096218+Mario-Mellek@users.noreply.github.com>
2023-05-10 00:09:19 +0000

    Wed, May 10, 2023, 2:09 AM +02:00

--- 1-last_digit.py
+++ 1-last_digit.py
@@ -4,12 +4,12 @@ number = random.randint(-10000, 10000)
 lastDigit = abs(number) % 10
 
 if number < 0:
-    lastDigit = abs(number) % 10
-print("Last digit of", number, "is", lastDigit)
+    lastDigit = -lastDigit
+print("Last digit of", number, "is", lastDigit, end=" ")
 
 if lastDigit > 5:
-    print(" and is greater than 5")
+    print("and is greater than 5")
 elif lastDigit == 0:
-    print(" and is 0")
+    print("and is 0")
 else:
-    print(" and is less than 6 and not 0")
+    print("and is less than 6 and not 0")
