c83d8da79c5bb761b85312cd16e064c0910a8369
Mario Mellek <107096218+Mario-Mellek@users.noreply.github.com>
2023-05-10 02:52:10 +0000

    Wed, May 10, 2023, 4:52 AM +02:00

--- functions/print_tebahpla.py
+++ functions/print_tebahpla.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 
-def reversePrint():
-    for i in range(25, -1, -1):
-        print("{}".format(chr(i + ord('A')) if i % 2 == 0 else chr(i + ord('a'))), end='')
-
-reversePrint()
+for i in range(25, -1, -1):
+    letter = chr(i + ord("A")) if i % 2 == 0 else chr(i + ord("a"))
+    print("{}".format(letter), end="")
