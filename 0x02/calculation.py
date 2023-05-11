e27c4d1d96073ad6a23d37548e252f2e57ce4b68
Mario Mellek <107096218+Mario-Mellek@users.noreply.github.com>
2023-05-11 22:03:25 +0000

    Fri, May 12, 2023, 12:03 AM +02:00

--- 0x02/calculation.py
+++ 0x02/calculation.py
@@ -3,7 +3,7 @@ import calculator_1 as cal
 a = 10
 b = 5
 
-print("{} + {} = {}".format(a, b, add(a, b)))
-print("{} - {} = {}".format(a, b, sub(a, b)))
-print("{} * {} = {}".format(a, b, mul(a, b)))
-print("{} / {} = {}".format(a, b, div(a, b)))
+print("{} + {} = {}".format(a, b, cal.add(a, b)))
+print("{} - {} = {}".format(a, b, cal.sub(a, b)))
+print("{} * {} = {}".format(a, b, cal.mul(a, b)))
+print("{} / {} = {}".format(a, b, cal.div(a, b)))
