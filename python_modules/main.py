58b64071650e4c154dad0039b71853457f3e2921
Mario Mellek <107096218+Mario-Mellek@users.noreply.github.com>
2023-05-11 17:24:20 +0000

    Thu, May 11, 2023, 7:24 PM +02:00

--- python_modules/main.py
+++ python_modules/main.py
@@ -1,13 +1,21 @@
 import module
 import math
 import datetime
+import random
 
 print(dir(module))
 print(dir(math))
 module.prime(2)
 print("Built-in math.trunc {} = {}".format(5.2343, math.trunc(5.2343)))
 print(dir(datetime))
-print(datetime.time())
+print(datetime.date.today())
+print(datetime.datetime.now())
+print(datetime.datetime.now().time())
+
+print(\n ------------------ \n)
+
+li = ["Car", "Bike", "Jet", "Boat"]
+print(random.choice(li))
 
 # from module import prime
 # prime(2)
