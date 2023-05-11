a5416be8b8b98b2aa8a91979cb34bd7b7a59d9ba
Mario Mellek <107096218+Mario-Mellek@users.noreply.github.com>
2023-05-11 17:18:47 +0000

    Thu, May 11, 2023, 7:18 PM +02:00

--- python_modules/main.py
+++ python_modules/main.py
@@ -1,10 +1,13 @@
 import module
 import math
+import datetime
 
 print(dir(module))
 print(dir(math))
 module.prime(2)
 print("Built-in math.trunc {} = {}".format(5.2343, math.trunc(5.2343)))
+print(dir(datetime))
+print(datetime.time())
 
 # from module import prime
 # prime(2)
