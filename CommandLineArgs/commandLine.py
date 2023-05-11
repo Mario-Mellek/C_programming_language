d42af1be59bcf79c05a8dfdff83fdecd4f4c9224
Mario Mellek <107096218+Mario-Mellek@users.noreply.github.com>
2023-05-11 19:02:15 +0000

    Thu, May 11, 2023, 9:02 PM +02:00

--- CommandLineArgs/commandLine.py
+++ CommandLineArgs/commandLine.py
@@ -3,7 +3,13 @@ import sys
 
 print("\nNumber of Args: {}\n".format(len(sys.argv) - 1))
 
+arg = sys.argv[1]
 n = len(sys.argv)
 
 for x in range(1, n):
-    print("Arg {}= {}".format(x ,sys.argv[x]))
+    print("Arg {}= {}".format(x, sys.argv[x]))
+
+if arg == "Mario":
+    print(f"Welcome {arg}")
+else:
+    print("Not Allowed")
