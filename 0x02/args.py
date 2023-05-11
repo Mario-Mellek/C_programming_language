f9fb82c8fbf6b0e34fbbc202c30d8a80e7297e9b
Mario Mellek <107096218+Mario-Mellek@users.noreply.github.com>
2023-05-11 22:26:52 +0000

    Fri, May 12, 2023, 12:26 AM +02:00

---
+++ 0x02/args.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+
+    args = sys.argv
+    count = len(args)
+
+    print()
+
+    for i in range(1, count):
+        print("{}: {}".format(, i, argv[i]))
