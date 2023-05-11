d868bbcb15fe8e6472485ff79763a258474e6a80
Mario Mellek <107096218+Mario-Mellek@users.noreply.github.com>
2023-05-11 17:09:44 +0000

    Thu, May 11, 2023, 7:09 PM +02:00

--- python_modules/main.py
+++ python_modules/main.py
@@ -1,10 +1,13 @@
-# import module
-# module.prime(2)
+import module
+import math
+
+print(dir(module))
+print(dir(math))
+module.prime(2)
+print("Built-in math.turnc 5.2343 = {}".format(math.trunc(5.2343)))
 
 # from module import prime
 # prime(2)
 
 # import module as mod
 # mod.prime(2)
-
-
