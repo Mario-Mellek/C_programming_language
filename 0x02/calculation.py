44de52eeae7d7aee36ac7989b91db53655cf9414
Mario Mellek <107096218+Mario-Mellek@users.noreply.github.com>
2023-05-11 22:01:48 +0000

    Fri, May 12, 2023, 12:01 AM +02:00

--- 0x02/calculation.py
+++ 0x02/calculation.py
@@ -1,1 +1,9 @@
 import calculator_1 as cal
+
+a = 10
+b = 5
+
+print("{} + {} = {}".format(a, b, add(a, b)))
+print("{} - {} = {}".format(a, b, sub(a, b)))
+print("{} * {} = {}".format(a, b, mul(a, b)))
+print("{} / {} = {}".format(a, b, div(a, b)))
