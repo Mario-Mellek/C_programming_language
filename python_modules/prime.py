2561d962da7dac57434c90333f3f101b3d52f912
Mario Mellek <107096218+Mario-Mellek@users.noreply.github.com>
2023-05-11 16:23:42 +0000

    Thu, May 11, 2023, 6:23 PM +02:00

--- python_modules/prime.py
+++ python_modules/prime.py
@@ -1,4 +1,15 @@
 def prime(n):
-    if n =< 1:
+    if n <= 1:
         print("Prime Numbers are greater than 1")
         return
+    if n > 1:
+        for i in range(2, n):
+            if (n % i) == 0:
+                print(f"{n} is a prime number")
+                return
+            else:
+                print(f"{n} isn't a prime number")
+                return
+
+
+prime(2)
