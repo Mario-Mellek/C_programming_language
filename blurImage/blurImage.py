cf241f82fa1ce0822d15f81dfc9cb5b17ea85faa
Mario Mellek <107096218+Mario-Mellek@users.noreply.github.com>
2023-05-12 13:50:41 +0000

    Fri, May 12, 2023, 3:50 PM +02:00

--- blurImage/blurImage.py
+++ blurImage/blurImage.py
@@ -1,10 +1,6 @@
 from PIL import Image, ImageFilter
 
-
-print(dir(Image))
-
 before = Image.open("image.jpg")
 after = before.filter(ImageFilter.BoxBlur(20))
-after = Image.rotate(90)
 
-after.save("out.jpg")
+after.save("blurred.jpg")
