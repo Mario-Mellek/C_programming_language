cb4dca5f8db2e0b95e1646a6ca361fe0520a48d4
Mario Mellek <107096218+Mario-Mellek@users.noreply.github.com>
2023-05-12 14:04:27 +0000

    Fri, May 12, 2023, 4:04 PM +02:00

--- blurImage/blurImage.py
+++ blurImage/blurImage.py
@@ -1,6 +1,13 @@
 from PIL import Image, ImageFilter
+import sys
+
+args = sys.argv
+
+if len(args) != 2:
+    print("Usage: Enter the degree of blurring as a command line argument")
+    sys.exit(1)
 
 before = Image.open("image.jpg")
-after = before.filter(ImageFilter.BoxBlur(20))
+after = before.filter(ImageFilter.BoxBlur(int(args[1])))
 
 after.save("blurred.jpg")
