26d09f23fa941b1eb0bfca9ff1812ce6f6906155
Mario-Mellek <mario.samy266@gmail.com>
2023-05-14 16:40:08 +0300

    edited scores.py to accept commandLine Args

--- python-practice/list/scores.py
+++ python-practice/list/scores.py
@@ -1,3 +1,15 @@
+#!/usr/bin/python3
+from sys import argv, exit
+
+# Gets the number of subjects and store it in the variable n
+if len(argv) != 2 or argv[1].isdigit() != True:
+    print("Enter the number of subjects as a commandLine argument")
+    exit(1)
+else:
+    n = int(argv[1])
+
+
+# main function, the param n is the number of subjects
 def main(n):
     scores = []
     getScores(scores, n)
@@ -5,6 +17,7 @@ def main(n):
     printAverage(scores)
 
 
+# Gets each subject score in ints
 def getScores(scores, n):
     for i in range(n):
         while True:
@@ -15,6 +28,7 @@ def getScores(scores, n):
                 print("Invalid Entry, Try again")
 
 
+# Prints back the scores
 def printScores(scores):
     j = 1
     for i in scores:
@@ -23,9 +37,10 @@ def printScores(scores):
         print(i)
 
 
+# Prints the Average
 def printAverage(scores):
     avg = sum(scores) / len(scores)
     print(f"Average score: %{avg:.2f}")
 
 
-main(5)
+main(n)
